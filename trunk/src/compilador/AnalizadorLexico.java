265ae36d44c8b64773ff69e0b448f2901fa198f9
danieloop <danieloop@d5486b97-d743-0410-b6b1-6f80a54cf937>
2008-01-20 17:09:38 +0000

    Añadida funcion token

--- trunk/src/compilador/AnalizadorLexico.java
+++ trunk/src/compilador/AnalizadorLexico.java
@@ -29,24 +29,65 @@ public class AnalizadorLexico {
 	 * Posición en la que se encuentra el analizador léxico dentro de la cadena a analizar.
 	 */
 	private int pos;
-	
+	/**
+	 * Codigo asignado al lexema si se ha reconocido como válido. P.E. asig para la cadena ':='.
+	 */
+	private String token;
 	/** Constructor del analizador lexico.
 	 * 
 	 * @param file Nombre del fichero cuyo lexico va a ser analizado.
 	 */
 	public AnalizadorLexico(String file){
 		this.archivo = AuxFun.getTextoFichero(file);
+		this.archivo = this.archivo.toLowerCase();
 		this.numLinea = 1;
 		this.lex = "";
+		this.token = "";
 		this.estado = 0;
 		this.pos = 0;
 	}
 	
 	public void transita(int state){
-		lex = lex.concat(archivo.substring(pos,1));
+		this.lex = lex.concat(archivo.substring(pos,1));
+		this.pos++;
+		this.estado = state;
 		
 	}
-	
+	public void token(String lexema){
+		if(lexema == "("){
+			this.token = "lparen";
+		}else if(lexema == ")"){
+			this.token = "rparen";
+		}else if(lexema == ":"){
+			this.token = "dosp";
+		}else if(lexema == ";"){
+			this.token = "semip";
+		}else if(lexema == "+"){
+			this.token = "suma";
+		}else if(lexema == "-"){
+			this.token = "resta";
+		}else if(lexema == "*"){
+			this.token = "prod";
+		}else if(lexema == "div"){
+			this.token = "div";
+		}else if(lexema == "mod"){
+			this.token = "mod";
+		}else if(lexema == "/"){
+			this.token = "DivReal";
+		}else if(lexema == "="){
+			this.token = "eq";
+		}else if(lexema == "<>"){
+			this.token = "ne";
+		}else if(lexema == ">"){
+			this.token = "gt";
+		}else if(lexema == ">="){
+			this.token = "ge";
+		}else if(lexema == "<"){
+			this.token = "lt";
+		}else if(lexema == "<="){
+			this.token = "le";
+		}
+	}
 	
 	
 	/**
