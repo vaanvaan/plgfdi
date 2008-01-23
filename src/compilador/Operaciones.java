1332f6595372b0a701e97e0f3fdc2f2f6bf4cdd7
ssjivan <ssjivan@d5486b97-d743-0410-b6b1-6f80a54cf937>
2008-01-23 23:08:15 +0000

    Esta clase, es una basurilla que me quedaba de IAIC, jejeje que la he cambiado para que me sirva de main y me ejecute una prueba sobre la máquina P. Asi que este es el main, ahora tb subo el fichero de programa prueba.

---
+++ src/compilador/Operaciones.java
@@ -0,0 +1,14 @@
+package compilador;
+import java.util.ArrayList;
+import java.util.Stack;
+
+
+public class Operaciones {
+
+	public static void main(String[] args) {
+		String path = "D:/pru.txt";
+		Maquina_P maq = new Maquina_P();
+		maq.inicializa_mem_pru();
+		maq.procesa_programa();
+	}
+}
