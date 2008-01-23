b33f5cc6e3a444a74d5116329561ce3a1f16cedc
ssjivan <ssjivan@d5486b97-d743-0410-b6b1-6f80a54cf937>
2008-01-23 23:05:20 +0000

    Value Object que he creado para poder procesar los pares de  [ accion | operando ] // ya os imaginais, separar y almacenar apila 3 en "apila" y "3"

---
+++ src/compilador/ComandoVO.java
@@ -0,0 +1,19 @@
+package compilador;
+
+public class ComandoVO {
+	private String accion;
+	private String operando;
+	
+	public String getAccion() {
+		return accion;
+	}
+	public void setAccion(String accion) {
+		this.accion = accion;
+	}
+	public String getOperando() {
+		return operando;
+	}
+	public void setOperando(String operando) {
+		this.operando = operando;
+	}
+}
