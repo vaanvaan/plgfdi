badabcbb8ec95d91df9cdd155a87767673328bb7
danieloop <danieloop@d5486b97-d743-0410-b6b1-6f80a54cf937>
2008-01-26 14:01:39 +0000

    + funciones del sintactico

--- src/compilador/AnalizadorSintactico.java
+++ src/compilador/AnalizadorSintactico.java
@@ -36,15 +36,60 @@ public class AnalizadorSintactico {
 	}
 	
 	public void declaraciones(boolean err0){
-		
+		boolean errh1 = false;
+		boolean err1 = false;
+		int dir1 = 0;
+		int dirh1 = 0;
+		this.declaracion(tipo,listaID,tipoDecl);
+		errh1 = false;
+		dirh1 = 0;
+		this.compara(";");
+		this.declaracionesR(dirh1,errh1,err1,dir1);
+		err0=err1;
+		// TODO añadir a la lista
 	}
 	
-	public void identificador(String lex){
-		anaLex.scanner();
-		lex = anaLex.getLex();
+	public void declaracionesR(int dirh0,boolean errh0,boolean err0,int dir0){
+		// TODO hay que ver en cual de los 2 casos estas, en el que hay una declaracion o no hay nada
+		if(this.compara(" ")){  
+			int dirh1 = 0;
+			boolean errh1 = false;
+			boolean err1 = false;
+			int dir1 = 0;
+			this.declaracion(tipo,listaID,tipoDecl);
+			dirh1 = dirh0 + 1;
+			errh1 = (errh0 || existeID(listaID));
+			this.compara(";");
+			this.declaracionesR(dirh1, errh1, err1, dir1);
+			err0 = err1;
+			dir0 = dir1;
+			// TODO añadir a la lista
+		}else{
+			err0=errh0;
+			dir0=dirh0;
+		}
+		
 	}
-	public void proposicion_compuesta(boolean err0,boolean err1){}
-	public void declaraciones(){}
+	public void declaracion(String tipo0,lista listaID0,String tipoDecl0){
+		if(this.compara(" ")){
+			String tipo1 = "";
+			lista listaID1;
+			this.variable(tipo1,listaID1);
+			tipo0=tipo1;
+			listaID0=listaID1;
+			tipoDecl0="var";
+		}else{
+			String tipo1 = "";
+			lista listaID1;
+			this.constante(tipo1,listaID1);
+			tipo0=tipo1;
+			listaID0=listaID1;
+			tipoDecl0="var";
+		}
+	}
+	
+	
+	
 	
 	
 	/** Funcion que compara un string dado con el siguiente elemento lexico a analizar.
