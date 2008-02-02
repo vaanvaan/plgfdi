7cdf4d5dffa5e7d0212f38a412d69b672b950e08
danieloop <danieloop@d5486b97-d743-0410-b6b1-6f80a54cf937>
2008-02-02 12:09:05 +0000

    cambio menor en TODO

--- src/compilador/Compiler.java
+++ src/compilador/Compiler.java
@@ -26,6 +26,7 @@ public class Compiler {
 		 * Para que funcione se necesita pasar un argumento por parametro al llamar a compilar.
 		 */
 		if(args.length==2){
+			// TODO pillar 2º path para generar el codigo
 			compilador.compile(args[0].toString());
 		}else{
 			System.out.println("Se necesitan dos paths como argumento, 1 con el path del fuente y otro con el destino" +
