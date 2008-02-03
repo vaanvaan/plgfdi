cba5109679e0d35b63161cd07ccd7b9949367606
ssjivan <ssjivan@d5486b97-d743-0410-b6b1-6f80a54cf937>
2008-02-03 15:01:44 +0000

    Añadidos comentarios javadoc

--- trunk/src/maquinaP/ComandoVO.java
+++ trunk/src/maquinaP/ComandoVO.java
@@ -1,18 +1,51 @@
 package maquinaP;
 
+/**
+ * 
+ * @author PLG GRUPO Nº13
+ * 
+ * Clase que nos proporciona Value Objects para poder procesar las instrucciones
+ * de la maquina P.
+ * 
+ */
 public class ComandoVO {
 	private String accion;
 	private String operando;
-	
+
+	/**
+	 * Obtiene la acción de la instrucción
+	 * 
+	 * @return la acción que debe realizar la máquina P
+	 */
 	public String getAccion() {
 		return accion;
 	}
+
+	/**
+	 * Fija la acción
+	 * 
+	 * @param accion
+	 *            nombre de la acción
+	 */
 	public void setAccion(String accion) {
 		this.accion = accion;
 	}
+
+	/**
+	 * Obtiene el operando
+	 * 
+	 * @return el operando asociado a la acción
+	 */
 	public String getOperando() {
 		return operando;
 	}
+
+	/**
+	 * Fija el operando de la acción
+	 * 
+	 * @param operando
+	 *            el operando correspondiente.
+	 */
 	public void setOperando(String operando) {
 		this.operando = operando;
 	}
