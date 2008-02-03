026acce1953804076d7b48058d13bcf3058dc1bd
danieloop <danieloop@d5486b97-d743-0410-b6b1-6f80a54cf937>
2008-02-03 17:04:15 +0000

    debuggeando

--- trunk/src/compilador/Compiler.java
+++ trunk/src/compilador/Compiler.java
@@ -38,26 +38,28 @@ public class Compiler {
 		System.out.print("Ruta de salida> ");
 		r2 = scanEntrada.nextLine();
 		compilador.compile(r1, r2);
-		String orden;
-		do{
-			System.out.println("Desea ejecutar la maquina-P ahora?");
-			orden = scanEntrada.nextLine();
-		}while(orden.toLowerCase().compareTo("si")!=0&&orden.toLowerCase().compareTo("no")!=0);
-		if(orden.toLowerCase().compareTo("si")==0){
-			String ord;
-			boolean debug=false;
+		if(!Global.getError()){
+			String orden;
 			do{
-				System.out.print("Modo debug si/no> ");
-				ord = scanEntrada.nextLine();
-			}while(ord.toLowerCase().compareTo("si")!=0&&ord.toLowerCase().compareTo("no")!=0);
-			if(ord.toLowerCase().compareTo("si")==0){
-				debug = true;
+				System.out.println("Desea ejecutar la maquina-P ahora?");
+				orden = scanEntrada.nextLine();
+			}while(orden.toLowerCase().compareTo("si")!=0&&orden.toLowerCase().compareTo("no")!=0);
+			if(orden.toLowerCase().compareTo("si")==0){
+				String ord;
+				boolean debug=false;
+				do{
+					System.out.print("Modo debug si/no> ");
+					ord = scanEntrada.nextLine();
+				}while(ord.toLowerCase().compareTo("si")!=0&&ord.toLowerCase().compareTo("no")!=0);
+				if(ord.toLowerCase().compareTo("si")==0){
+					debug = true;
+				}
+				Maquina_P maq = new Maquina_P();
+				maq.setPath(r2);
+				maq.inicializa_mem_pru();
+				maq.setDebug(debug); // <--- modo debug
+				maq.procesa_programa();
 			}
-			Maquina_P maq = new Maquina_P();
-			maq.setPath(r2);
-			maq.inicializa_mem_pru();
-			maq.setDebug(debug); // <--- modo debug
-			maq.procesa_programa();
 		}
 		
 	}
